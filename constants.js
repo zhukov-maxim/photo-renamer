40cbf9f2cbdca96431405c8e9ccba94f5ebd7cd8
Maxim Zhukov <zhkv.mxm@gmail.com>
2018-01-21 15:39:01 +0700

    Prepare Angular-based version of the project

--- constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  DATE_DELIMITER: '-',
-  FILENAME_DELIMITER: ' - ',
-  NEW_FOLDER_NAME: 'Untitled'
-};
--- main.js
+++ main.js
@@ -1,80 +1,43 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
+const { app, BrowserWindow } = require('electron');
+require('electron-reload')(__dirname);
 
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-const path = require('path');
-const url = require('url');
-
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-});
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let win;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    fullscreenable: false,
-    maximizable: false,
-    width: 480,
-    minWidth: 320,
+  win = new BrowserWindow({
+    width: 600,
     height: 600,
-    minHeight: 300,
-    backgroundColor: '#f2f2f2',
-    title: 'Photo Renamer',
-    acceptFirstMouse: true
+    backgroundColor: '#ffffff',
+    icon: `file://${__dirname}/dist/assets/logo.png`
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+  // win.loadURL(`file://${__dirname}/dist/index.html`);
+  win.loadURL('http://localhost:4200/');
 
-  // Open the DevTools in development mode.
-  if (process.env.NODE_ENV === 'development') {
-    const electronDevtoolsInstaller = require('electron-devtools-installer');
+  // uncomment below to open the DevTools.
+  win.webContents.openDevTools();
 
-    electronDevtoolsInstaller.default(electronDevtoolsInstaller.VUEJS_DEVTOOLS)
-      .then((name) => {
-        console.log(`Added Extension: ${name}`);
-        mainWindow.webContents.openDevTools();
-      })
-      .catch(err => console.log('An error occurred: ', err));
-  }
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+  // Event when the window is closed.
+  win.on('closed', function() {
+    win = null;
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Create window on electron intialization
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  app.quit();
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
+app.on('window-all-closed', function() {
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('activate', function() {
+  // macOS specific close process
+  if (win === null) {
+    createWindow();
+  }
+});
